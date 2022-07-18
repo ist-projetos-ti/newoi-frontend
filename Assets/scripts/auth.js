1b6b54917816606917287988ac06a396ed2ddbe7
Giovanni Nespoli <ggnespoli@gmail.com>
2022-07-18 11:04:01 -0300

    fix: css structure

---
+++ Assets/scripts/auth.js
@@ -0,0 +1,34 @@
+function Auth(e) { 
+    
+    // Informações do input 
+    const userName = document.querySelector('input[name="user"]').value;
+    const password = document.querySelector('input[name="pwd"]').value;
+    const companyId = document.querySelectorAll('option:checked')[0].value
+
+    // API url
+    const baseUrl = 'http://localhost:3333/sessions';
+    
+    //Requisição de POST na API para login
+     fetch(baseUrl, {
+    method: "POST",
+    headers: {"Content-Type":" application/json; charset=UTF-8"},
+    body: JSON.stringify({
+        //Corpo da requisição
+        company: companyId,
+        email: userName,
+        password: password
+        })
+    }).then( function (response){
+        
+        //Se o status code enviado pela API for 200, redireciona para DashBoard
+        if(response.status == 200){
+            window.AuthAction()
+        } else{
+            //Se não, retorna erro na autenticação
+            window.StopAction()
+        }
+
+    }).catch(function(error) {
+       error = console.log('An error ocurred!');
+    });
+}
